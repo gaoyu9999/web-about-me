42bd3416e561667fc4e34253fd20f5f033b4edaa
gao_gaoyu@travelsky.com.cn <gao_gaoyu@travelsky.com.cn>
2023-03-22 16:23:51 +0800

    1.1.1

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:gaoyu9999/web-about-me.git master:gh-pages
 
 cd -
